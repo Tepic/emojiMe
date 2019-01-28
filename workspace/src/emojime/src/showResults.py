ca40ef55c9e0e5ba1c72ea7aae9ab13acec91086
Tepic <tepic.milan@yahoo.com>
2019-01-28 17:12:55 +0100

    Prototype to independently periodically refresh output image is done. It is left over to interconnect with ROS. This code could be used as a prototype in evaluation procedure.

--- workspace/src/emojime/src/showResults.py
+++ workspace/src/emojime/src/showResults.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import os
 from PIL import Image, ImageDraw, ImageFont
 
 # show image
@@ -7,8 +8,53 @@ import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import math
 
-def createResults(resize_coeff_satisfied,resize_coeff_shocked,resize_coeff_neutral,resize_coeff_bored,backgroung_color):
-	canvas = Image.new('RGB', (1024, 768), color = backgroung_color)
+import numpy as np
+import cv2
+
+import threading
+import time
+
+global satisfied
+global shocked
+global neutral
+global bored
+global stopFlag
+	
+satisfied = 25
+shocked   = 25
+neutral   = 25
+bored     = 25
+stopFlag = False
+
+def getColor(percentages):
+	satisfied = percentages[0]
+
+	ordered = [percentages[0], percentages[1], percentages[2], percentages[3]]
+
+	ordered.sort()
+	value_1 = ordered[3] # MAX
+	value_2 = ordered[2]
+	value_3 = ordered[1]
+	value_4 = ordered[0] # MIN
+
+	if(value_1==satisfied):
+		if(value_1>=2*value_2):
+			return green
+		if(value_1>=1.5*value_2):
+			return yellow
+		if(value_1>value_2):
+			return orange
+	else:
+		return red
+
+def createResults(percentages):
+	resize_coeff_satisfied = percentages[0]
+	resize_coeff_shocked   = percentages[1]
+	resize_coeff_neutral   = percentages[2]
+	resize_coeff_bored     = percentages[3]
+
+	background_color = getColor(percentages)
+	canvas = Image.new('RGB', (1024, 768), color = background_color)
 	yellow_text = (255, 255, 0)
 	black_text = (0,0,0)
 	#fnt = ImageFont.truetype('/Library/Fonts/Arial.ttf', 15)
@@ -59,31 +105,59 @@ def createResults(resize_coeff_satisfied,resize_coeff_shocked,resize_coeff_neutr
 	bored_location = (896-4*18,552) #896-72
 	bored_text = str(resize_coeff_bored*50.0)
 
-	canvas.paste(satisfied_png, satisfied_png_location)
+	canvas.paste(satisfied_png, satisfied_png_location,satisfied_png)
 	background.text(satisfied_location, satisfied_text, font=emoji_font, fill=black_text)
 
-	canvas.paste(shocked_png, shocked_png_location)
+	canvas.paste(shocked_png, shocked_png_location,shocked_png)
 	background.text(shocked_location, shocked_text, font=emoji_font, fill=black_text)
 
-	canvas.paste(neutral_png, neutral_png_location)
+	canvas.paste(neutral_png, neutral_png_location,neutral_png)
 	background.text(neutral_location, neutral_text, font=emoji_font, fill=black_text)
 
-	canvas.paste(bored_png, bored_png_location)
+	canvas.paste(bored_png, bored_png_location,bored_png)
 	background.text(bored_location, bored_text, font=emoji_font, fill=black_text)
 	 
 	canvas.save('Result_Image.png')
+	canvas.close()
+
+def getNewResults():
+	global satisfied
+	global shocked
+	global neutral
+	global bored
+	global stopFlag
+	global percentages
+    
+	while(True):
+		satisfied = float(raw_input("Satisfied: "))
+		shocked   = float(raw_input("Shocked: "))
+		neutral   = float(raw_input("Neutral: "))
+		bored     = float(raw_input("Bored: "))
+		percentages = [satisfied,shocked,neutral,bored]
+		if(raw_input("Do you want to terminate? ")=='yes'):
+			stopFlag = True
+			break
+	
 
 green = (34,139,34)
-black = (0,0,0)
+yellow = (255,255,0)
 orange = (255,165,0)
 red = (255,0,0)
-yellow = (255,255,0)
+black = (0,0,0)
 
-satisfied = 55
-shocked = 5.4
-neutral = 19
-bored = 25.6
+percentages = [satisfied,shocked,neutral,bored]
+# createResults(percentages)
 
-createResults(satisfied,shocked,neutral,bored, orange)
-#output = Image.open("Result_Image.png")
-#output.show()
+input_thread = threading.Thread(target=getNewResults)
+input_thread.start()
+
+pressed = 0
+while(not stopFlag):#pressed!=101): # press e for exit
+
+	#percentages = [satisfied,shocked,neutral,bored]
+
+	createResults(percentages)
+	output = cv2.imread("Result_Image.png")
+	cv2.imshow('Results',output)
+	cv2.waitKey(1000)
+	#pressed = cv2.waitKey(0) & 0xFF
