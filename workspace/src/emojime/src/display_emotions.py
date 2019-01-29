cde9c2ca29abdb16144c14a58ed04d687259f6a8
Tepic <tepic.milan@yahoo.com>
2019-01-29 22:13:01 +0100

    Display image is also done. Now it is left to change CNN model layers setup. In addition, it would be good to change names of files, so they have appropriate and logical naming (purpose to understandable from the name).

--- workspace/src/emojime/src/display_emotions.py
+++ workspace/src/emojime/src/display_emotions.py
@@ -5,18 +5,43 @@ import sys
 import serial
 import time
 import numpy as np
+import showResults
+
+# threading
+import threading
+import time
+import sys
 
 # import messages
 from std_msgs.msg import Int16MultiArray
 
 #detected_faces_previously = 0
 
+global satisfied_input
+global shocked_input
+global neutral_input
+global bored_input
+
+satisfied_input = 0
+shocked_input   = 0
+neutral_input   = 0
+bored_input     = 0
+
+global counter
+counter = 0
+
+global __DEBUG__
+__DEBUG__ = False
+
 def callback_emotions(emotions):
+    global counter
+    global __DEBUG__
+
     data = list(emotions.data)
     detected_faces = data[0]
     emotions = data[1:]
-    print detected_faces
-    print emotions
+    #print detected_faces
+    #print emotions
 
     # check these things out
     print('Emotions')
@@ -35,7 +60,7 @@ def callback_emotions(emotions):
     neutral = emotions.count(6)
     #print neutral
 
-    anngry = angry+disguisted+fearful
+    angry = angry+disguisted+fearful
 
     emoticons_percentage = []
     emoticons_percentage.append(angry)
@@ -53,18 +78,44 @@ def callback_emotions(emotions):
     print('======================================================================')
     print(str(emoticons_percentage[0])+'\t'+str(emoticons_percentage[1])+'\t'+str(emoticons_percentage[2])+'\t'+str(emoticons_percentage[3])+'\t\t'+str(emoticons_percentage[4]))
 
+    satisfied_input = emoticons_percentage[1]
+    shocked_input   = emoticons_percentage[0]+emoticons_percentage[3]
+    neutral_input   = emoticons_percentage[4]
+    bored_input     = emoticons_percentage[2]
+
+    if(__DEBUG__):
+        counter = counter+1
+        print("counter: %i" % counter)
+
+    if(__DEBUG__):
+        print("Update results")
+    showResults.setNewResults(satisfied_input,shocked_input,neutral_input,bored_input)
+    new_data_available = False
+
+def display():
+    while(True):
+        print("executing")
+        showResults.main()
 
 def main():
+    global new_data_available
+
     rospy.init_node('display_emotions')
     rospy.Subscriber("/emotions", Int16MultiArray , callback_emotions)
-    while not rospy.is_shutdown():
+    while not rospy.is_shutdown():        
         rospy.spin()
         
     cv2.destroyAllWindows()
 
 if __name__ == '__main__':
-    try:
+    try:        
+        display_thread = threading.Thread(target=display)
+        display_thread.daemon = True
+
         print("Just started")
+        display_thread.start()
+        
         main()
+        
     except rospy.ROSInterruptException:
         pass
--- workspace/src/emojime/src/showResults.py
+++ workspace/src/emojime/src/showResults.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-
+# ~/Documents/tepic/Git/emojiMe/workspace/src/emojime/src
 import os
 from PIL import Image, ImageDraw, ImageFont
 
@@ -13,6 +13,7 @@ import cv2
 
 import threading
 import time
+import sys
 
 global satisfied
 global shocked
@@ -20,11 +21,25 @@ global neutral
 global bored
 global stopFlag
 	
-satisfied = 25
-shocked   = 25
-neutral   = 25
-bored     = 25
+satisfied = 55
+shocked   = 15
+neutral   = 15
+bored     = 15
 stopFlag = False
+
+# text_color
+yellow_text = (255, 255, 0)
+black_text = (0,0,0)
+
+# background
+green = (34,139,34)
+yellow = (255,255,0)
+orange = (255,165,0)
+red = (255,0,0)
+black = (0,0,0)
+
+y_offset = -65
+wait_1s = 1000
 
 def getColor(percentages):
 	satisfied = percentages[0]
@@ -44,8 +59,31 @@ def getColor(percentages):
 			return yellow
 		if(value_1>value_2):
 			return orange
+		else:
+			return red
 	else:
 		return red
+
+def createResults_time(percentages,minutes,seconds):
+	createResults(percentages)	
+	canvas = Image.open('Result_Image.png')
+	background= ImageDraw.Draw(canvas)
+	clock_font = ImageFont.truetype('/usr/share/fonts/truetype/liberation/LiberationMono-BoldItalic.ttf', 64)
+	if(seconds<10):
+		if(minutes<10):
+			clock_text = '0'+str(minutes)+':0'+str(seconds)
+		else:
+			clock_text = str(minutes)+':0'+str(seconds)
+	else:
+		if(minutes<10):
+			clock_text = '0'+str(minutes)+':'+str(seconds)
+		else:
+			clock_text = str(minutes)+':'+str(seconds)
+
+	clock_location = (800,680)
+	background.text(clock_location, clock_text, font=clock_font, fill=black_text)
+	canvas.save('Result_Image.png')
+	canvas.close()
 
 def createResults(percentages):
 	resize_coeff_satisfied = percentages[0]
@@ -55,11 +93,14 @@ def createResults(percentages):
 
 	background_color = getColor(percentages)
 	canvas = Image.new('RGB', (1024, 768), color = background_color)
-	yellow_text = (255, 255, 0)
-	black_text = (0,0,0)
-	#fnt = ImageFont.truetype('/Library/Fonts/Arial.ttf', 15)
-	emoji_font = ImageFont.truetype('/usr/share/fonts/truetype/liberation/LiberationMono-Italic.ttf', 48)
 	background= ImageDraw.Draw(canvas)
+
+	title_font = ImageFont.truetype('/usr/share/fonts/truetype/liberation/LiberationMono-BoldItalic.ttf', 36)
+	title_text = "emojiME - audiance response"
+	title_location = (240,40)
+	background.text(title_location, title_text, font=title_font, fill=black_text)
+
+	emoji_font = ImageFont.truetype('/usr/share/fonts/truetype/liberation/LiberationMono-BoldItalic.ttf', 48)
 
 	resize_coeff_satisfied = resize_coeff_satisfied*2.0/100
 	satisfied_png = Image.open('./emojies/satisfied.png', 'r')
@@ -68,9 +109,9 @@ def createResults(percentages):
 	if(new_dimensions[0]<30):
 		new_dimensions = (30,30)
 	satisfied_png = satisfied_png.resize(new_dimensions, Image.ANTIALIAS)
-	satisfied_png_location = (160-int(math.floor(new_dimensions[0]/2)), 400-int(math.floor(new_dimensions[1]/2)))
-	satisfied_location = (128-18,552) #256-18
-	satisfied_text = str(resize_coeff_satisfied*50.0) # it was scalled x2, up to 200%, and then divided by 100
+	satisfied_png_location = (160-int(math.floor(new_dimensions[0]/2)), 400-int(math.floor(new_dimensions[1]/2))+y_offset)
+	satisfied_location = (128-18-30,502+y_offset) #256-18
+	satisfied_text = str(resize_coeff_satisfied*50.0)+'%' # it was scalled x2, up to 200%, and then divided by 100
 
 	resize_coeff_shocked = resize_coeff_shocked*2.0/100
 	shocked_png = Image.open('./emojies/shocked.png', 'r')
@@ -79,9 +120,9 @@ def createResults(percentages):
 	if(new_dimensions[0]<30):
 		new_dimensions = (30,30)
 	shocked_png = shocked_png.resize(new_dimensions, Image.ANTIALIAS)
-	shocked_png_location = (225+160-int(math.floor(new_dimensions[0]/2)), 400-int(math.floor(new_dimensions[1]/2)))
-	shocked_location = (384-2*18,552) #384-36
-	shocked_text = str(resize_coeff_shocked*50.0)
+	shocked_png_location = (225+160-int(math.floor(new_dimensions[0]/2)), 400-int(math.floor(new_dimensions[1]/2))+y_offset)
+	shocked_location = (384-2*18-30,502+y_offset) #384-36
+	shocked_text = str(resize_coeff_shocked*50.0)+'%'
 
 	resize_coeff_neutral = resize_coeff_neutral*2.0/100
 	neutral_png = Image.open('./emojies/neutral.png', 'r')
@@ -90,9 +131,9 @@ def createResults(percentages):
 	if(new_dimensions[0]<30):
 		new_dimensions = (30,30)
 	neutral_png = neutral_png.resize(new_dimensions, Image.ANTIALIAS)
-	neutral_png_location = (2*235+160-int(math.floor(new_dimensions[0]/2)), 400-int(math.floor(new_dimensions[1]/2)))
-	neutral_location = (640-3*18,552) #640-54
-	neutral_text = str(resize_coeff_neutral*50.0)
+	neutral_png_location = (2*235+160-int(math.floor(new_dimensions[0]/2)), 400-int(math.floor(new_dimensions[1]/2))+y_offset)
+	neutral_location = (640-3*18-30,502+y_offset) #640-54
+	neutral_text = str(resize_coeff_neutral*50.0)+'%'
 
 	resize_coeff_bored = resize_coeff_bored*2.0/100
 	bored_png = Image.open('./emojies/bored.png', 'r')
@@ -101,9 +142,9 @@ def createResults(percentages):
 	if(new_dimensions[0]<30):
 		new_dimensions = (30,30)
 	bored_png = bored_png.resize(new_dimensions, Image.ANTIALIAS)
-	bored_png_location = (3*235+160-int(math.floor(new_dimensions[0]/2)), 400-int(math.floor(new_dimensions[1]/2)))
-	bored_location = (896-4*18,552) #896-72
-	bored_text = str(resize_coeff_bored*50.0)
+	bored_png_location = (3*235+160-int(math.floor(new_dimensions[0]/2)), 400-int(math.floor(new_dimensions[1]/2))+y_offset)
+	bored_location = (896-4*18-30,502+y_offset) #896-72
+	bored_text = str(resize_coeff_bored*50.0)+'%'
 
 	canvas.paste(satisfied_png, satisfied_png_location,satisfied_png)
 	background.text(satisfied_location, satisfied_text, font=emoji_font, fill=black_text)
@@ -119,6 +160,7 @@ def createResults(percentages):
 	 
 	canvas.save('Result_Image.png')
 	canvas.close()
+	#return canvas
 
 def getNewResults():
 	global satisfied
@@ -129,35 +171,77 @@ def getNewResults():
 	global percentages
     
 	while(True):
-		satisfied = float(raw_input("Satisfied: "))
-		shocked   = float(raw_input("Shocked: "))
-		neutral   = float(raw_input("Neutral: "))
-		bored     = float(raw_input("Bored: "))
-		percentages = [satisfied,shocked,neutral,bored]
-		if(raw_input("Do you want to terminate? ")=='yes'):
-			stopFlag = True
-			break
+		# input = raw_input("Do you want to terminate [yes = y]? ")
+		# if(input=='y' or input=='yes'):
+		# 	stopFlag = True
+		# 	#break
+		# else:
+		# 	stopFlag = False
+		input = raw_input("satisfied, shocked, neutral, bored: ")
+		input     = input.split(",")
+		#satisfied = float(raw_input("Satisfied: "))
+		# shocked   = float(raw_input("Shocked: "))
+		# neutral   = float(raw_input("Neutral: "))
+		# bored     = float(raw_input("Bored: "))
+		if(len(input) == 4):
+			satisfied = float(input[0])
+			shocked   = float(input[1])
+			neutral   = float(input[2])
+			bored     = float(input[3])
+			setNewResults(satisfied,shocked,neutral,bored)#,stopFlag)
+			#percentages = [satisfied,shocked,neutral,bored]
+
+def setNewResults(satisfied_input,shocked_input,neutral_input,bored_input):#,status=False):
+	global satisfied
+	global shocked
+	global neutral
+	global bored
+	#global stopFlag
+	global percentages
+    
+	satisfied = satisfied_input
+	shocked   = shocked_input
+	neutral   = neutral_input
+	bored     = bored_input
+	#stopFlag  = status
+	percentages = [satisfied,shocked,neutral,bored]
+	#print(stopFlag)
 	
-
-green = (34,139,34)
-yellow = (255,255,0)
-orange = (255,165,0)
-red = (255,0,0)
-black = (0,0,0)
-
 percentages = [satisfied,shocked,neutral,bored]
 # createResults(percentages)
 
-input_thread = threading.Thread(target=getNewResults)
-input_thread.start()
 
-pressed = 0
-while(not stopFlag):#pressed!=101): # press e for exit
 
-	#percentages = [satisfied,shocked,neutral,bored]
+# display_thread = threading.Thread(target=setNewResults)
+# display_thread.start()
 
-	createResults(percentages)
-	output = cv2.imread("Result_Image.png")
-	cv2.imshow('Results',output)
-	cv2.waitKey(1000)
-	#pressed = cv2.waitKey(0) & 0xFF
+def main():	
+	pressed = 0
+	minutes = 0
+	seconds = 0
+
+	while(not stopFlag):
+		createResults_time(percentages,minutes,seconds)
+		output = cv2.imread("Result_Image.png")
+		cv2.imshow('Results',output)
+		cv2.waitKey(wait_1s)
+		seconds = seconds+1
+		if(seconds==60):
+			seconds = 0
+			minutes = minutes+1
+			
+	print("Shutting down")
+
+if __name__ == '__main__':
+	try:
+		input_thread = threading.Thread(target=getNewResults)
+		
+		#enables the thread to kill itself after the end of the program has been reached
+		# this line must be executed before the thread has started
+		input_thread.daemon = True
+
+		input_thread.start()
+
+		main()
+	except (KeyboardInterrupt, SystemExit):
+	    sys.exit()
